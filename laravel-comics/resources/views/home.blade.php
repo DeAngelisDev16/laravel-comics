2040ad61b35d4b1372a610bb79e67f6bc2b42fe7
Shelbot77 <alessiodeangelis021@gmail.com>
2023-02-08 16:25:10 +0100

    SOS NON MI PRENDE I COMANDI

--- laravel-comics/resources/views/home.blade.php
+++ laravel-comics/resources/views/home.blade.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>laravel-comics</title>
-</head>
-
-<body>
-    @include('partials.header')
+@section('main-content')
 
 
-    <h2>Home</h2>
 
-</body>
 
-</html>
+@endsection
